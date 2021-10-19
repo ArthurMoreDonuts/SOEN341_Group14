62aee27b9f9fb4ff2513b06c98f5325bbbb973bb
philippebertrand22 <bertrand.philippe22@gmail.com>
2021-10-19 19:39:51 -0400

    Slight Modifications to login page
    Commiting the code on this branch until I am finished editing the layout, then will copy to correct branch

--- main_display/src/components/login/login.js
+++ main_display/src/components/login/login.js
@@ -11,9 +11,6 @@ export class Login extends React.Component {
       <div className="base-container" ref={this.props.containerRef}>
         <div className="header">Login</div>
         <div className="content">
-          <div className="image">
-            <img src={loginImg} />
-          </div>
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
