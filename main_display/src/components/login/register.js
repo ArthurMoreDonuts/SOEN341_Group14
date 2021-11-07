0108a7477a97613cd4b0e391177b2fbec25dd0b0
Daniel Gelfand <danielgelfand@outlook.com>
2021-11-07 06:23:16 -0500

    Implemented user registration with backend connectivity
    #15 #73

--- main_display/src/components/login/register.js
+++ main_display/src/components/login/register.js
@@ -4,7 +4,74 @@ import React from "react";
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+
+
+
   }
+ state ={
+     username: "",
+     email: "",
+     password: ""
+
+
+     };
+
+handleNewUsername(e)
+{
+this.state=({username: e, email:this.state.email, password: this.state.password});
+
+
+}
+
+handleNewPassword(e)
+{
+this.state=({username: this.state.username, email:this.state.email, password: e});
+
+}
+handleNewEmail(e)
+{
+this.state=({username: this.state.username, email: e, password: this.state.password});
+
+}
+
+handleRegistration()
+{
+
+        console.log("this.state");
+
+
+
+        const http = new XMLHttpRequest();
+
+
+        http.open("POST","http://localhost:8080/api/New_User");
+        http.setRequestHeader("Content-Type" , "application/json");
+
+
+        http.send(JSON.stringify(this.state));
+
+        //http.send(JSON.stringify(questionObject));
+        http.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 201) {
+
+
+              alert('Registration succesful! ');
+
+               var userInfo;
+               userInfo = JSON.parse(this.responseText);
+               console.log(userInfo);
+
+                }
+
+            if(this.status==500)
+            {
+                alert('Registration Failed! Username/Email already taken! ');
+            }
+
+        }
+
+}
+
 
   render() {
     return (
@@ -14,25 +81,36 @@ export class Register extends React.Component {
         { /* <div className="image">
             <img src={loginImg} />
           </div>*/}
-          <div className="form">
+          <div className="form"  >
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+                  <input type="text"
+                  name="username"
+                  placeholder="username"
+                  onChange={e =>  this.handleNewUsername(e.target.value)}
+
+              />
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
+                    <input type="text"
+                    name="email"
+                    placeholder="email"
+                    onChange={e =>  this.handleNewEmail(e.target.value)}
+                    />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+                    <input type="text"
+                    name="password"
+                    placeholder="password"
+                    onChange={e =>  this.handleNewPassword(e.target.value)}
+                    />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
-            Register
-          </button>
+          <button type="button" className="btn" onClick= {()=>this.handleRegistration()} > Register</button>
         </div>
       </div>
     );
