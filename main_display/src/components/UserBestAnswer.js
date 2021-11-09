01dde9e6c61738859d178562a3ecd1a7b71f2f5d
philippebertrand22 <bertrand.philippe22@gmail.com>
2021-11-09 14:48:36 -0500

    Setting up the js files
    first commit im not done setting up the files, going to need help

---
+++ main_display/src/components/UserBestAnswer.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Checkbox from './checkbox';
+
+const SELECT = ["Select as Best Answer"];
+
+class UserBestAnswer extends Component {
+    state = {
+        checkboxes: SELECT.reduce(
+            (options, option) => ({
+                ...options,
+                [option]: false
+            }),
+            {}
+        )
+    };
+
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+
+        Object.keys(this.state.checkboxes)
+        .filter(checkbox => this.state.checkboxes[checkbox])
+        .forEach(checkbox => {
+            console.log(checkbox, "was selected.");
+        });
+    };
+
+
+    createCheckbox = option => (
+        <Checkbox
+            label = {option}
+            isSelected = {this.state.checkboxes[option]}
+            onCheckboxChange = {this.handleCheckboxChange}
+            key = {option}
+    />
+
+    );
+
+    createCheckboxes = () => SELECT.map(this.createCheckbox);
+
+    render() {
+        return(
+            <div className="container">
+                <div className="row mt-5">
+                    <div className="col-sm-12">
+                        <form onSubmit = {this.handleFormSubmit}>
+                            {this.createCheckboxes()}
+
+                            <button type = "submit" className = "btn btn-primary">
+                                Select
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserBestAnswer;
