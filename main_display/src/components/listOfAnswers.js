a4fe5a1c33bc5f87e335e40f96429b4a4039157b
Daniel Gelfand <danielgelfand@outlook.com>
2021-12-01 20:34:59 -0500

    added list of answers

---
+++ main_display/src/components/listOfAnswers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataService from "../services/question.service";
+import axios from 'axios';
+import './styling.css';
+import { useHistory, useParams } from 'react-router-dom'
+import { withRouter } from "react-router";
+import PostButton from "./PostButton";
+
+
+export class listOfAnswers extends React.Component{
+
+}
+export default listOfAnswers;
